fe76b036f610873b788026150c321639c328d7d6
tspersian <tspersian@gmail.com>
2021-07-31 12:35:17 +0430

    upload and download artifact

--- Dockerfile
+++ Dockerfile
@@ -17,7 +17,7 @@ RUN mkdir -p /android/sdk && \
 
 RUN cd /android/sdk && \
     yes | ./tools/bin/sdkmanager --licenses && \
-    ./tools/bin/sdkmanager 'build-tools;27.0.3' 'build-tools;28.0.3' platform-tools 'platforms;android-28' 'platforms;android-27' 'platforms;android-26' 'ndk-bundle'
+    ./tools/bin/sdkmanager 'build-tools;27.0.3' 'build-tools;29.0.0' 'build-tools;28.0.3' platform-tools 'platforms;android-28' 'platforms;android-29' 'platforms;android-27' 'platforms;android-26' 'ndk-bundle'
 
 RUN lynx -listonly -dump https://jenkins.mono-project.com/view/Xamarin.Android/job/xamarin-android-linux/lastSuccessfulBuild/Azure/ | grep -o "https://.*/Azure/processDownloadRequest/xamarin-android/xamarin.android-oss_v.*-Release.tar.bz2" > link.txt && \
     curl -L $(cat link.txt) \
--- build.sh
+++ build.sh
@@ -1,5 +1,48 @@
-#!/bin/bash
-# Build docker image
-# author: Claudiu Chiticariu Constantin (chiticariu at gmail.com)
+#!/usr/bin/env bash
 
-docker build -t chiticariu/xamarin-android .
+clear
+DIR="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
+NAME="$(basename "$DIR")"
+
+if [ -f "$DIR/$NAME/$NAME.Android.csproj" ]; then
+    DIR = "$DIR/$NAME";
+elif [ -f "$DIR/$NAME.Android.csproj" ]; then
+    DIR = "$DIR";
+fi
+
+echo "Android file found in $DIR"
+
+build () {
+    if [[ "$(docker images -q tspersian/xamarin-android 2> /dev/null)" == "" ]]; then
+        printf "\nDocker image not found\n"
+        read -p "Do you like to install it? (y/n) " -n 1 -r
+        echo 
+        if [[ $REPLY =~ ^[Yy]$ ]]
+            sudo docker build -t tspersian/xamarin-android . &>> build.log  
+            exec $0
+        then
+            printf "Script requires docker image with Mono, JDK, NDK and SDK installed you can use Dockerfile or install it manually\n"
+            printf "To build your Docker image please run \`docker build -t tspersian/xamarin-android\` .\n"
+        fi
+    else
+        sudo docker run -v $DIR/:/test tspersian/xamarin-android msbuild test/$NAME.Android/$NAME.Android.csproj /restore /p:AndroidSdkDirectory=/android/sdk /p:AndroidBuildApplicationPackage=true
+    fi 
+    exit
+}
+
+if [ ! -f "$NAME.sln" ]; then
+printf "\n"
+    echo "Solution file not found (.sln)"
+printf "\n"
+    echo "Please move script to project path where android folder in xamarin project exists(e.g. /home/{{user}}/Documents/Projects/{{Name}}/)"
+    exit
+else
+    printf "\n"
+    read -p "Project '$NAME' found continue? (y/n) " -n 1 -r
+    echo 
+    if [[ $REPLY =~ ^[Yy]$ ]]
+        build
+    then
+        exit
+    fi
+fi
